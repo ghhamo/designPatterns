7127f61570094a67249f625f1978de35ba293d27
Hamlet <hamoghukasyan98@gmail.com>
2024-09-12 22:07:40 +0400

    singleton done

---
+++ singleton/src/main/java/job/Singleton.java
@@ -0,0 +1,19 @@
+package job;
+
+public class Singleton {
+    private static Singleton s;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (s == null) {
+            synchronized (Singleton.class) {
+                if (s == null) {
+                    s = new Singleton();
+                }
+            }
+        }
+        return s;
+    }
+}
---
+++ singleton/src/main/java/job/SingletonMain.java
@@ -0,0 +1,7 @@
+package job;
+
+public class SingletonMain {
+    public static void main(String[] args) {
+        Singleton instance = Singleton.getInstance();
+    }
+}
